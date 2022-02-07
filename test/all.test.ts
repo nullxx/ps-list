e5ccba2afd93869594104db69bd5cbfdca0c7b19
Jon Lara <jon.laratrigo@gmail.com>
2022-02-07 19:40:30 +0100

    Update tests

--- test/all.test.ts
+++ test/all.test.ts
@@ -23,4 +23,24 @@ describe('getProcessList', () => {
       expect(processes[0]).toBeInstanceOf(LinuxProcess)
     }
   })
+
+  test('properties', async () => {
+    const processes = await getProcessList()
+    if (processes.length === 0) return
+
+    const process = processes[0]
+    if (process instanceof MacOSProcess) {
+      expect(process).toHaveProperty('pid')
+      expect(process).toHaveProperty('name')
+    } else if (process instanceof WindowsProcess) {
+      expect(process).toHaveProperty('PID')
+      expect(process).toHaveProperty('Image Name')
+      expect(process).toHaveProperty('Session Name')
+      expect(process).toHaveProperty('Mem Usage')
+      expect(process).toHaveProperty('Session#')
+    } else if (process instanceof LinuxProcess) {
+      expect(process).toHaveProperty('pid')
+      expect(process).toHaveProperty('name')
+    }
+  })
 })
