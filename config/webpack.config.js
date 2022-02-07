8a6e506feabd0b59dd52358faa34144e84ef9a8f
Jon Lara <jon.laratrigo@gmail.com>
2022-02-07 19:36:06 +0100

    first commit

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist/umd'),
     filename: 'index.js',
-    library: 'exampleTypescriptPackage',
+    library: '@nullx/ps-list',
     libraryTarget: 'umd',
     globalObject: 'this',
   },
